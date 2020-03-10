6b65d21a9c437099ad31454c208d5effe57ee226
batuhan <batuhanonur29@gmail.com>
2020-03-10 21:11:43 +0300

    vue router

---
+++ routing/src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
